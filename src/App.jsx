39d5e2bc3279b6114d30c1e78fb99264dbed1c35
TeslaSeven7 <alex.aubrier@epitech.eu>
2023-09-26 15:39:45 +0200

    updated hero's size

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,8 @@ function App() {
 	const ref = useRef();
 	return (
 		<div className='App'>
-			<div className='container-fluid'>
-				<div className='container col-xxl-8 px-4 py-5'>
+			<div className='container-fluid d-flex flex-column align-items-center'>
+				<div className='col-xxl-8 px-4 py-5'>
 					<div className='row flex-lg-row-reverse align-items-center g-5 py-5'>
 						<div className='col-lg-6'>
 							<h1 className='display-5 fw-bold lh-1 mb-3 text-start'>
